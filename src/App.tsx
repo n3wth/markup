cb246f581cf426d10f3185844c5d6da37512d182
n3wth <newth@google.com>
2026-04-17 05:21:19 -0700

    chore: silence react-hooks/refs for buildCommands call with rationale (#196)

--- src/App.tsx
+++ src/App.tsx
@@ -581,7 +581,10 @@ function App() {
         <CommandPalette
           onClose={() => setShowCommandPalette(false)}
           commands={buildCommands(
+            // editorRef is dereferenced only inside command action callbacks, never during render.
+            // eslint-disable-next-line react-hooks/refs
             { activeSession, activeAgents, agentsPaused, sidebarCollapsed, isLocalhost, hasUser: !!user, editorRef: editorRef as React.RefObject<{ getText: () => string } | null> },
+            // eslint-disable-next-line react-hooks/refs
             { setShowTemplatePicker, handleTogglePause, setShowConfigurator, setSidebarCollapsed, setShowExperiments, resetToHome, setMessages, toast, signOut, uid, now },
           )}
         />
